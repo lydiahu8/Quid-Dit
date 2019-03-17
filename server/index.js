9b9d061d2403f715797d729b70ab9a0ec5f70cc5
lydiahu8 <lydiahu8@gmail.com>
2019-03-16 19:24:46 -0700

    Implement addOneGame in server

--- server/index.js
+++ server/index.js
@@ -1,23 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {
-  getAllGames
+  getAllGames,
+  addOneGame
 } = require('../database');
 
 const app = express();
+let port = 3000;
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.json());
 
 app.get('/games', (req, res) => {
   getAllGames((err, data) => {
     if (err) {
-      res.sendStatus(500);
+      res.status(500).send(err);
     } else {
       res.status(200).send(data);
     }
   });
 });
 
-app.listen(3000, function () {
+app.post('/games', (req, res) => {
+  const {
+    body
+  } = req;
+  addOneGame(body, (err) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(body);
+    }
+  });
+});
+
+app.listen(port, () => {
   console.log('listening on port 3000!');
 });
