d097cbb33e945ec05195abeb486a3a1ec8626794
lydiahu8 <lydiahu8@gmail.com>
2019-03-16 19:06:31 -0700

    Implement getAllGames in server

--- server/index.js
+++ server/index.js
@@ -1,29 +1,23 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-// var items = require('../database-mysql');
-// var items = require('../database-mongo');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {
+  getAllGames
+} = require('../database');
 
-var app = express();
+const app = express();
 
-// UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../client/dist'));
 
-// UNCOMMENT FOR ANGULAR
-// app.use(express.static(__dirname + '/../angular-client'));
-// app.use(express.static(__dirname + '/../node_modules'));
-
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
+app.get('/games', (req, res) => {
+  getAllGames((err, data) => {
+    if (err) {
       res.sendStatus(500);
     } else {
-      res.json(data);
+      res.status(200).send(data);
     }
   });
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('listening on port 3000!');
 });
-
