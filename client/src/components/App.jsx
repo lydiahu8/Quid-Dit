f586a8840676b8037ab30e3be02cbd482ff6f510
lydiahu8 <lydiahu8@gmail.com>
2019-03-16 23:24:49 -0700

    Implement moving background with goalPosts

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -20,6 +20,33 @@ class App extends React.Component {
       x_axis: 1
     }
 
+    let goalPosts = [
+      {
+        height: Math.floor(Math.random() * 11) + 2,
+        position: 4
+      },
+      {
+        height: Math.floor(Math.random() * 11) + 2,
+        position: 9
+      },
+      {
+        height: Math.floor(Math.random() * 11) + 2,
+        position: 14
+      },
+      {
+        height: Math.floor(Math.random() * 11) + 2,
+        position: 19
+      },
+      {
+        height: Math.floor(Math.random() * 11) + 2,
+        position: 24
+      },
+      {
+        height: Math.floor(Math.random() * 11) + 2,
+        position: 29
+      },
+    ];
+
     // Sets the color of the character to yellow
     canvas[character.y_axis][character.x_axis] = '#e6b800';
 
@@ -27,19 +54,38 @@ class App extends React.Component {
       score: 0,
       canvas: canvas,
       character: character,
+      goalPosts: goalPosts
     };
 
-    this.charDrop = setInterval(() => this.handleCharDrop(), 200);
+    // Sets a time for the character to drop
+    this.charDrop = setInterval(() => this.handleCharMove(), 500);
   }
 
-  handleCharDrop() {
+  // Changes the position of the character when falling until the ground
+  handleCharMove() {
     let canvas2 = [];
     let height2 = 15;
+    let goalPosts2 = this.state.goalPosts.slice();
 
     while (height2 > 0) {
       let innerCanvas2 = new Array(30);
       canvas2.push(innerCanvas2.fill('#220033'));
       height2--;
+    }
+
+    for (let i = 0; i < goalPosts2.length; i++) {
+      goalPosts2[i].position -= 1;
+      if (goalPosts2[i].position < 0) {
+        goalPosts2[i].height = Math.floor(Math.random() * 11) + 2;
+        goalPosts2[i].position = 29;
+      }
+    }
+
+    // Creates the goal posts
+    for (let i = 0; i < goalPosts2.length; i++) {
+      for (let j = 0; j < goalPosts2[i].height; j++) {
+        canvas2[14 - j][goalPosts2[i].position] = '#800000';
+      }
     }
 
     let char = this.state.character;
