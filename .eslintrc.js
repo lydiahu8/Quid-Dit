db25ac0da1b6a80bf8bb7497e7cfe5d781faea4c
lydiahu8 <lydiahu8@gmail.com>
2019-03-16 17:04:54 -0700

    Update webpack and eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: 'airbnb',
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "react/prop-types": 0,
+    "react/destructuring-assignment": 0,
+    "no-console": "off",
+  }
+};
