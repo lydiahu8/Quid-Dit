e0d6812eeb441faa5e23edc979578205b5c9b00a
lydiahu8 <lydiahu8@gmail.com>
2019-03-18 10:52:05 -0700

    Add addOneUser and getAllGamesByUser

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const config = require('../config/db.config');
 
 const db = mysql.createConnection(config);
 
+// Gets all games played by all users
 const getAllGames = (callback) => {
   const query = 'SELECT * FROM games;';
   db.query(query, function (err, results) {
@@ -14,9 +15,35 @@ const getAllGames = (callback) => {
   });
 };
 
+// Gets all games for one user
+const getAllGamesByUser = (id, callback) => {
+  const query = `SELECT * FROM games INNER JOIN users ON games.user_id = users.id WHERE games.user_id = ${id};`;
+  db.query(query, function (err, results) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    callback(null, results);
+  });
+};
+
+// Add one user
+const addOneUser = (user, callback) => {
+  const query = `INSERT into users (username, password)  VALUES (?, ?);`;
+  const params = [user.username, user.password];
+  db.query(query, params, (err) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+    callback(null);
+  });
+}
+
+//Add one game to games table
 const addOneGame = (game, callback) => {
-  const query = 'INSERT INTO games (score) VALUES (?);';
-  const params = [game.score];
+  const query = `INSERT INTO games (score, user_id) VALUES (?, ?);`;
+  const params = [game.score, game.user_id];
   db.query(query, params, (err) => {
     if (err) {
       callback(err);
@@ -28,5 +55,7 @@ const addOneGame = (game, callback) => {
 
 module.exports = {
   getAllGames,
+  getAllGamesByUser,
+  addOneUser,
   addOneGame
 };
