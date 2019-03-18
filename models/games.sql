5676e6d29d7e57c61a16fb9ffd92e8d9a26713ad
lydiahu8 <lydiahu8@gmail.com>
2019-03-18 02:00:51 -0700

    Update schema to include users table and move into models directory

--- models/games.sql
+++ models/games.sql
@@ -4,11 +4,21 @@ CREATE DATABASE quidditch;
 
 USE quidditch;
 
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  username varchar(25),
+  password varchar(25),
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE games (
   id int NOT NULL AUTO_INCREMENT,
   score integer NOT NULL,
+  user_id int,
+  FOREIGN KEY (user_id) REFERENCES users(id),
   PRIMARY KEY (id)
 );
+
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
