5b41fa98c4fcaf6bb210bd699def8e86a48aac69
lydiahu8 <lydiahu8@gmail.com>
2019-03-18 11:19:37 -0700

    Add host to config file

--- config/config.example.js
+++ config/config.example.js
@@ -1,6 +1,7 @@
 // Make a copy of this file, name it db.config.js and complete the information to connect to database
 
 module.exports = {
+  host: '',
   user: '',
   password: '',
   database: 'quidditch'
