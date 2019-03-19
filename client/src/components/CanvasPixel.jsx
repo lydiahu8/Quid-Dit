fc2bd9a3be4c838ba46028dae1142541a719df61
lydiahu8 <lydiahu8@gmail.com>
2019-03-19 14:58:47 -0700

    Update character, pole, and ring to s3 images

--- client/src/components/CanvasPixel.jsx
+++ client/src/components/CanvasPixel.jsx
@@ -12,7 +12,7 @@ const CanvasPixel = ({ pixelColor, score }) => {
     width: '2em',
     height: '2em',
     backgroundColor: score < 10 ? 'rgba(230, 230, 255, 0)' : score >= 10 && score < 20 ? 'rgba(255, 255, 179, 0)' : score >= 20 && score < 30 ? 'rgba(179, 230, 255,0)' : score >= 30 && score < 40 ? 'rgba(204, 255, 204, 0)' : 'rgba(255, 230, 230, 0)',
-    content: `url("https://i.pinimg.com/originals/15/f7/70/15f770c0e6a9d74fc987d026c36be2eb.png")`,
+    content: `url("https://s3.amazonaws.com/quitttt/harry_potter.png")`,
     zIndex: '99'
   };
 
@@ -20,7 +20,7 @@ const CanvasPixel = ({ pixelColor, score }) => {
     width: '2em',
     height: '2em',
     backgroundColor: score < 10 ? 'rgba(230, 230, 255, 0)' : score >= 10 && score < 20 ? 'rgba(255, 255, 179, 0)' : score >= 20 && score < 30 ? 'rgba(179, 230, 255,0)' : score >= 30 && score < 40 ? 'rgba(204, 255, 204, 0)' : 'rgba(255, 230, 230, 0)',
-    content: `url("http://i43.tinypic.com/kbc32v.png")`,
+    content: `url("https://s3.amazonaws.com/quitttt/ring.png")`,
     transform: 'scale(3)',
     zIndex: '90'
   }
@@ -29,7 +29,7 @@ const CanvasPixel = ({ pixelColor, score }) => {
     width: '2em',
     height: '2em',
     backgroundColor: score < 10 ? 'rgba(230, 230, 255, 0)' : score >= 10 && score < 20 ? 'rgba(255, 255, 179, 0)' : score >= 20 && score < 30 ? 'rgba(179, 230, 255,0)' : score >= 30 && score < 40 ? 'rgba(204, 255, 204, 0)' : 'rgba(255, 230, 230, 0)',
-    content: `url("https://ya-webdesign.com/images/wood-pole-png-4.png")`,
+    content: `url("https://s3.amazonaws.com/quitttt/pole.png")`,
   }
 
   return (
