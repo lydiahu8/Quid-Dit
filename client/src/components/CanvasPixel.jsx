39625fa60a306c8240d59ce9ee7ad1cc7453356e
lydiahu8 <lydiahu8@gmail.com>
2019-03-18 23:33:45 -0700

    Implement goalPosts coming from top and bottom

--- client/src/components/CanvasPixel.jsx
+++ client/src/components/CanvasPixel.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 
 const CanvasPixel = ({ pixelColor }) => {
+
   const style = {
-    width: '3em',
-    height: '3em',
+    width: '2em',
+    height: '2em',
     backgroundColor: pixelColor,
   };
 
   const styleChar = {
-    width: '3em',
-    height: '3em',
+    width: '2em',
+    height: '2em',
     backgroundColor: '#b3e6ff',
     content: `url("https://i.pinimg.com/originals/15/f7/70/15f770c0e6a9d74fc987d026c36be2eb.png")`,
-    // transform: 'scale(.5)',
   };
 
+
   const styleRing = {
-    width: '3em',
-    height: '3em',
+    width: '2em',
+    height: '2em',
     backgroundColor: '#b3e6ff',
     content: `url("http://i43.tinypic.com/kbc32v.png")`,
-    transform: 'scale(1.5)',
+    transform: 'scale(2)',
   }
 
   const stylePole = {
-    width: '3em',
-    height: '3em',
+    width: '2em',
+    height: '2em',
     backgroundColor: '#b3e6ff',
     content: `url("https://ya-webdesign.com/images/wood-pole-png-4.png")`,
-    // transform: 'scale(1.5)',
   }
 
   return (
