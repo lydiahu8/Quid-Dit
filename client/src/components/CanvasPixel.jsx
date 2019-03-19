b5adb0c3fcb6a963eb01a1186e41f31296b1cdef
lydiahu8 <lydiahu8@gmail.com>
2019-03-18 22:42:38 -0700

    Update pixel sizes for goalPosts

--- client/src/components/CanvasPixel.jsx
+++ client/src/components/CanvasPixel.jsx
@@ -1,27 +1,49 @@
 import React from 'react';
 
 const CanvasPixel = ({ pixelColor }) => {
-
-  const styleChar = {
-    width: '2em',
-    height: '2em',
-    backgroundColor: '#b3e6ff',
-    content: `url("https://i.pinimg.com/originals/15/f7/70/15f770c0e6a9d74fc987d026c36be2eb.png")`
-  };
-
   const style = {
-    width: '2em',
-    height: '2em',
+    width: '3em',
+    height: '3em',
     backgroundColor: pixelColor,
   };
+
+  const styleChar = {
+    width: '3em',
+    height: '3em',
+    backgroundColor: '#b3e6ff',
+    content: `url("https://i.pinimg.com/originals/15/f7/70/15f770c0e6a9d74fc987d026c36be2eb.png")`,
+    // transform: 'scale(.5)',
+  };
+
+  const styleRing = {
+    width: '3em',
+    height: '3em',
+    backgroundColor: '#b3e6ff',
+    content: `url("http://i43.tinypic.com/kbc32v.png")`,
+    transform: 'scale(1.5)',
+  }
+
+  const stylePole = {
+    width: '3em',
+    height: '3em',
+    backgroundColor: '#b3e6ff',
+    content: `url("https://ya-webdesign.com/images/wood-pole-png-4.png")`,
+    // transform: 'scale(1.5)',
+  }
 
   return (
     <div>
       {
-        pixelColor === '#e0d543' ? (
+        pixelColor === '#ffffe6' ? (
           <div style={styleChar} ></div >
         ) :
-          <div style={style} ></div >
+          pixelColor === '#f7d247' ? (
+            <div style={styleRing} ></div >
+          ) :
+            pixelColor === '#fffda8' ? (
+              <div style={stylePole} ></div >
+            ) :
+              <div style={style} ></div >
       }
     </div>
   );
